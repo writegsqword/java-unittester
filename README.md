f1043723a07b45d74371b08404df0821b04fefdc
Mana <arw007@ucsd.edu>
2024-10-10 22:01:34 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# java-unittester  
+
+## how use: 
+
+paste this into your main class or something  
+call TestMethod and pass in your args  
+probably only works with simple types, i dont care enough to cover all cases  
---
+++ snippet.java
@@ -0,0 +1,38 @@
+
+//rip 30 minutes of my life :sob:
+public static boolean TestMethod(String methodName, Object result, Object... args) {
+    try{
+        Method[] methods = MethodHandles.lookup().lookupClass().getMethods();
+        Method method = null;
+        //find method, wont work with overloads
+        for(Method m : methods) { 
+            if(m.getName() == methodName){
+                method = m;
+                break;
+            }
+        }
+        //muh static typed language
+        Object testResult = method.invoke(null, args);
+        try {
+            if(result.getClass().getName().equals("java.lang.Double")) {
+                //if this didnt fail then it probably is a double
+                //round to 2 digits
+                testResult = (double)Math.round(Double.parseDouble(testResult.toString()) * 100) / 100;
+            }
+            else if(result.getClass().getName().equals("java.lang.Integer")) {
+                testResult = Integer.parseInt(testResult.toString());
+            }
+            //pass
+            
+        }
+        catch(Exception e){ 
+            //pass
+        }
+        return (testResult.toString().equals(result.toString()));
+
+    } catch(Exception e){ 
+        e.printStackTrace();
+        return false;
+    }
+    
+}
