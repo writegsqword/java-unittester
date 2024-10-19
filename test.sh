1c8aeaa4ab224425a72036ce37d51f28a07fee24
Mana <arw007@ucsd.edu>
2024-10-19 00:46:10 -0700

    move to out

--- test.sh
+++ test.sh
@@ -2,3 +2,6 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk/bin
 newname="$(echo $1 | sed 's/\./_processed./g')"
 bash macro-sed.sh $1
 java $newname
+mkdir -p out
+mv $newname out
+mv out/$newname out/$1
